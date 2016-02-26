71555d0b2c34cf17f03d27e023c743157cd7b467
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-02-26 15:58:08 +0100

    tabgroup actionabr in extra module

---
+++ Resources/ui/tabgroup.actionbar.js
@@ -0,0 +1,27 @@
+module.exports = function(_event) {
+	if (Ti.Platform.osname == 'android') {
+		var АктйонБар = require('com.alcoapps.actionbarextras');
+		АктйонБар.setTitle('Tierstimmenarchiv');
+		АктйонБар.setFont('Helvetica-Bold');
+		АктйонБар.setSubtitle('des Museums für Naturkunde Berlin');
+		АктйонБар.displayUseLogoEnabled = false;
+		АктйонБар.setStatusbarColor(COLOR.BROWN);
+		АктйонБар.backgroundColor = COLOR.DARKGREEN;
+		var activity = _event.source.getActivity();
+		if (activity) {
+			activity.onCreateOptionsMenu = function(_menuevent) {
+				console.log(_menuevent.menu.apiName);
+				return;
+				var item = _menuevent.menu.add({
+					title : 'Aufnahme',
+					showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM,
+					icon : Ti.App.Android.R.drawable.ic_action_mic
+				});
+				item.addEventListener("click", function(_e) {
+					require('ui/recorder.window')().open();
+				});
+			};
+			activity.invalidateOptionsMenu();
+		}
+	}
+}; 
--- Resources/ui/tabgroup.js
+++ Resources/ui/tabgroup.js
@@ -25,32 +25,6 @@ module.exports = function() {
 		})],
 		activeTab : 1
 	});
-	$.addEventListener('open', function(_event) {
-		if (Ti.Platform.osname == 'android') {
-			var АктйонБар = require('com.alcoapps.actionbarextras');
-			АктйонБар.setTitle('Tierstimmenarchiv');
-			АктйонБар.setFont('Helvetica-Bold');
-			АктйонБар.setSubtitle('des Museums für Naturkunde Berlin');
-			АктйонБар.displayUseLogoEnabled = false;
-			АктйонБар.setStatusbarColor(COLOR.BROWN);
-			АктйонБар.backgroundColor = COLOR.DARKGREEN;
-			var activity = _event.source.getActivity();
-			if (activity) {
-				activity.onCreateOptionsMenu = function(_menuevent) {
-					return;
-					var item = _menuevent.menu.add({
-						title : 'Aufnahme',
-						showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM,
-						icon : Ti.App.Android.R.drawable.ic_action_mic
-					});
-					item.addEventListener("click", function(_e) {
-						require('ui/recorder.window')().open();
-					});
-				};
-				activity.invalidateOptionsMenu();
-			}
-		}
-	});
-
+	$.addEventListener('open', require('ui/tabgroup.actionbar'));
 	$.open();
 };
